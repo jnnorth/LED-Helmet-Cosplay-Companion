614bcc3b96ebf37d1692ad56814400a3a23e6f1b
aclum256 <aclum256@gmail.com>
2021-02-06 11:30:37 -0600

    Update main.cpp
    Added signal handler that will send a kill signal to all scripts opened by main process. Updated button handling in the following way:
    If a button on the main hand is pressed, the drawing will update.
    If a button on the main hand is released, the drawing will remain.
    If a button on the mod hand is pressed, the drawing will update.
    If a button on the mod hand is released, the drawing will update.
    While the signal handler has been tested, I have not yet tested the button handling due to time constraints.

--- Hardware/main.cpp
+++ Hardware/main.cpp
@@ -3,14 +3,27 @@
 #include <sys/wait.h>
 #include <unistd.h>
 #include <cstring>
+#include <signal.h>
 #include <chrono>
 #include <fstream>
 #include <time.h>
 
 int map[] = {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
-bool buttons[10];
-
+bool buttons_real[10];
+bool buttons_virtual[10];
 bool mouth_open;
+
+pid_t process[3];
+#define RECEIVER_1 0
+#define	RECEIVER_2 1
+#define DRAWING    3
+
+// Interrupt handler, closes drawing process and receivers
+void catch_int(int sig_num) {
+	for(int i = 0; i < 3; i++) {
+		kill(process[i], SIGKILL);
+	}
+}
 
 void draw_neutral() {
 	std::cout << "neutral\n";
@@ -84,7 +97,7 @@ bool read_files() { // returns true if update needed
 	bool temp_buttons[10];
 	int temp_map[15];
 	for(int i = 0; i < 10; i++) {
-		temp_buttons[i] = buttons[i];
+		temp_buttons[i] = buttons_real[i];
 	}
 	for(int i = 0; i < 15; i++) {
 		temp_map[i] = map[i];
@@ -126,34 +139,34 @@ bool read_files() { // returns true if update needed
 		read(pipefd[0], &readVal, sizeof(readVal));
 		if(readVal != 100) {
 			if(readVal >= 16) {
-				buttons[4] = true;
+				buttons_real[4] = true;
 				readVal -= 16;
 			}
 			else {
-				buttons[4] = false;
+				buttons_real[4] = false;
 			}
 			if(readVal >= 8) {
-				buttons[3] = true;
+				buttons_real[3] = true;
 				readVal -= 8;
 			}
 			else {
-				buttons[3] = false;
+				buttons_real[3] = false;
 			}
 			if(readVal >= 4) {
-				buttons[2] = true;
+				buttons_real[2] = true;
 				readVal -= 4;
 			}
 			else {
-				buttons[2] = false;
+				buttons_real[2] = false;
 			}
 			if(readVal >= 2) {
-				buttons[1] = true;
+				buttons_real[1] = true;
 				readVal -= 2;
 			}
 			else {
-				buttons[1] = false;
+				buttons_real[1] = false;
 			}
-			buttons[0] = (readVal == 1);
+			buttons_real[0] = (readVal == 1);
 		}
 		close(pipefd[0]);
 	}
@@ -194,41 +207,50 @@ bool read_files() { // returns true if update needed
 		read(pipefd[0], &readVal, sizeof(readVal));
 		close(pipefd[0]);
 		if(readVal >= 16) {
-			buttons[9] = true;
+			buttons_real[9] = true;
 			readVal -= 16;
 		}
 		else {
-			buttons[9] = false;
+			buttons_real[9] = false;
 		}
 		if(readVal >= 8) {
-			buttons[8] = true;
+			buttons_real[8] = true;
 			readVal -= 8;
 		}
 		else {
-			buttons[8] = false;
+			buttons_real[8] = false;
 		}
 		if(readVal >= 4) {
-			buttons[7] = true;
+			buttons_real[7] = true;
 			readVal -= 4;
 		}
 		else {
-			buttons[7] = false;
+			buttons_real[7] = false;
 		}
 		if(readVal >= 2) {
-			buttons[6] = true;
+			buttons_real[6] = true;
 			readVal -= 2;
 		}
 		else {
-			buttons[6] = false;
+			buttons_real[6] = false;
 		}
-		buttons[5] = (readVal == 1);
+		buttons_real[5] = (readVal == 1);
 	}
 	//TODO: Temperature sensor reading
 	//
 	bool ret_val = false;
-	for(int i = 0; i < 10; i++) {
-		if(buttons[i] != temp_buttons[i]) {
+	for(int i = 0; i < 5; i++) {
+		if(buttons_real[i] && !temp_buttons[i]) {
 			ret_val = true;
+			for(int j = 0; j < 10; j++) {
+				buttons_virtual[j] = buttons_real[j];
+			}
+		}
+	}
+	for(int i = 5; i < 10; i++) {
+		if(buttons_real[i] != temp_buttons[i]) {
+			ret_val = true;
+			buttons_virtual[i] = buttons_real[i];
 		}
 	}
 	for(int i = 0; i < 15; i++) {
@@ -243,23 +265,23 @@ void (*function_pointer[])() = {draw_neutral, draw_cheerful, draw_starry_eyed, d
 
 int get_drawing() {
 	int ret = 0;
-	if(buttons[0]) {
+	if(buttons_virtual[0]) {
 		ret = 0;
 	}
-	if(buttons[1]) {
+	if(buttons_virtual[1]) {
 		ret = 4;
 	}
-	if(buttons[2]) {
+	if(buttons_virtual[2]) {
 		ret = 8;
 	}
-	if(buttons[3]) {
+	if(buttons_virtual[3]) {
 		ret = 12;
 	}
 
-	if(buttons[6]) {
+	if(buttons_virtual[6]) {
 		ret++;
 	}
-	if(buttons[7]) {
+	if(buttons_virtual[7]) {
 		ret += 2;
 	}
 	return map[ret];
@@ -283,30 +305,18 @@ void update_table() {
 	}
 }
 
-void my_exec(std::string file) {
-	char to_open[file.size() + 1];
-	strcpy(to_open, file.c_str());
-	char * args[] = {to_open, NULL};
-	execv(to_open, args);
-}
-
-void fork_and_call(void (*func)()) {
-	pid_t ret = fork();
+void fork_and_draw(void (*func)()) {
+	pid_t ret = process[DRAWING] = fork();
+	// The drawing function can then call exec to open the python script which does the drawing
 	if(ret == 0) {
 		func();
 		exit(0);
 	}
-}
-
-void fork_and_exec(std::string file) {
-	pid_t ret = fork();
-	if(ret == 0) {
-		my_exec(file);
-	}
+	// Parent process must get drawing processes pid via pipe
 }
 
 void draw() {
-	fork_and_call((*function_pointer[get_drawing()]));
+	fork_and_draw((*function_pointer[get_drawing()]));
 }
 
 void load_settings() {
@@ -336,28 +346,54 @@ void load_settings() {
 		
 
 void setup() {
-	fork_and_call(draw_loading);
-	pid_t ret = fork();
+	for(int i = 0; i < 3; i++) {
+		process[i] = 0;
+	}
+	fork_and_draw(draw_loading);
+	pid_t ret = process[RECEIVER_1] = fork();
 	if(ret == 0) {
-		char * args[] = {"python3", "bluetooth_receiver_1.py", NULL};
+		std::string py3 = "python3";
+		std::string btr = "bluetooth_receiver_1.py";
+		char py3_c[py3.size()+1];
+		char btr_c[btr.size()+1];
+		strcpy(py3_c, py3.c_str());
+		strcpy(btr_c, btr.c_str());
+		char * args[] = {py3_c, btr_c, NULL};
 		execvp(args[0], args);
 		std::cerr << "Failure to open receiver 1\n";
 		exit(0);
 	}
-	ret = fork();
+	ret = process[RECEIVER_2] = fork();
 	if(ret == 0) {
-		char * args[] = {"python3", "bluetooth_receiver_2.py", NULL};
+		std::string py3 = "python3";
+		std::string btr = "bluetooth_receiver_2.py";
+		char py3_c[py3.size()+1];
+		char btr_c[btr.size()+1];
+		strcpy(py3_c, py3.c_str());
+		strcpy(btr_c, btr.c_str());
+		char * args[] = {py3_c, btr_c, NULL};
 		execvp(args[0], args);
 		std::cerr << "failure to open receiver 2\n";
 		exit(0);
 	}
 	load_settings();
 	for(int i = 0; i < 10; i++) {
-		buttons[i] = false;
+		buttons_virtual[i] = false;
+		buttons_real[i] = false;
 	}
 	for(int i = 0; i < 15; i++) {
 		map[i] = i;
 	}
+
+	// Set up interrupt handler
+	struct sigaction sa;
+	sigset_t mask_set;
+	sa.sa_flags = SA_RESTART;
+	sa.sa_handler = catch_int;
+	sigfillset(&mask_set);
+	sigdelset(&mask_set, SIGINT);
+	sa.sa_mask = mask_set;
+	sigaction(SIGINT, &sa, NULL);
 }
 
 int get_ms_time() {
@@ -366,8 +402,18 @@ int get_ms_time() {
 	return (int)(ms_time % 1000);
 }
 void debug_buttons() {
+	std::cout << "real: ";
 	for(int i = 0; i < 10; i++) {
-		if(buttons[i]) {
+		if(buttons_real[i]) {
+			std::cout << "1";
+		}
+		else {
+			std::cout << "0";
+		}
+	}
+	std::cout << "\nvirtual: ";
+	for(int i = 0; i < 10; i++) {
+		if(buttons_virtual[i]) {
 			std::cout << "1";
 		}
 		else {
