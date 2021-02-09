023c8d2dde7ad04e100be12ef0800dac04c31ced
aclum256 <aclum256@gmail.com>
2021-02-09 15:34:01 -0600

    Update bluetooth_receiver_2.py
    Now tries several ports and can inform the main process if the connection was successful or not.

--- Hardware/bluetooth_receiver_2.py
+++ Hardware/bluetooth_receiver_2.py
@@ -1,30 +1,43 @@
-# Write your code here :-)
 import bluetooth
 import sys
 from time import time
 
 
 bd_addr = 'ac:67:b2:37:4c:86'
-port = 1
 
-try:
-    s = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
-    print("Connecting...")
-    s.connect((bd_addr, port))
-    print("Connection successful")
-    original_stdout = sys.stdout
-    while True:
-        mystr = ""
-        received = s.recv(1024)
-        mystr = received.decode('utf-8')
-        print(mystr)
-        ms_time = int(time() * 1000) % 100
-        while 30 < ms_time and ms_time < 70:
-            ms_time = int(time() * 1000) % 100
-        f = open('/home/pi/Hardware/blue_intput_2_2.txt', 'w')
-        f.write(mystr + '\n')
+def bt_connect(port):
+    try:
+        s = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
+        s.connect((bd_addr, port))
+        while 30 < int(time() * 1000) % 100 < 70:
+            x = 0 # do nothing, must have some code here to avoid indentation error
+        f = open('/home/pi/Hardware/blue_input_1.txt', 'w')
+        f.write("1")
         f.close()
-        sys.stdout = original_stdout
-except bluetooth.btcommon.BluetoothError as err:
-    print(err)
-    pass
+        original_stdout = sys.stdout
+        while True:
+            mystr = ""
+            received = s.recv(1024)
+            mystr = received.decode('utf-8')
+            print(mystr)
+            ms_time = int(time() * 1000) % 100
+            while 30 < ms_time < 70:
+                ms_time = int(time() * 1000) % 100
+                
+            f = open('/home/pi/Hardware/blue_input_2.txt', 'w')
+            f.write(mystr + '\n')
+            f.close()
+            sys.stdout = original_stdout
+    except bluetooth.btcommon.BluetoothError as err:
+        print(err)
+        pass
+
+for port_num in range(1, 32):
+    bt_connect(port_num)
+
+while 30 < int((time() * 1000) % 100) < 70:
+    x = 0
+print("writing failure text")
+f = open('/home/pi/Hardware/blue_input_1.txt', 'w')
+f.write("-1")
+f.close()
